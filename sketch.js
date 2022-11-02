7611cd007550c8ba5d87fef21f1aebd8b55a7e75
ParkGyeongJu <anauddl414@naver.com>
2022-11-02 10:21:33 +0900

    flower의 가장 기본 형태 만들기

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let flower;
+
+
 function setup() {
   createCanvas(400, 400);
+  flower = new walker();
 }
 
 function draw() {
-  background(220);
+  background(50);
+  
+  flower.show();
+}
+
+class walker {
+  constructor() {
+  translate(200, 200);  
+}
+  show(){
+  translate(200, 200);  
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+  ellipse(0, 30, 20, 80);
+  rotate(PI/5);
+  }
+  }
 }
