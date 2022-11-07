1fd127197e623716831f479ab19d7db1bd9cc0ed
ParkGyeongJu <anauddl414@naver.com>
2022-11-07 23:21:56 +0900

    이번엔 새롭게 vector안쓰고 만듬

--- sketch.js
+++ sketch.js
@@ -1,58 +1,112 @@
+// let flower;
+
+
+// function setup() {
+//   createCanvas(400, 400);
+//   ellipseMode(CENTER);
+//   flower = new walker(200,200,0.5);
+// }
+
+// function draw() {
+//   background(50);
+  
+//   flower.show();
+  
+//   if(mouseIsPressed){
+//   let wind = createVector(5,0);
+//   flower.applyForce(wind);
+//   }
+  
+  
+//   let gravity = createVector(0,0.1);
+  
+//   let weight=p5.Vector.mult(gravity, flower.mass);
+  
+//   flower.applyForce(weight);
+  
+//   flower.update();
+// }
+
+// class walker {
+  
+  
+  
+//   constructor(x,y,m) {
+//   this.pos = createVector(width/2,height/2);
+//   this.vel = createVector(0,0);
+//   this.acc = createVector(0,0);
+//   this.mass=m;
+  
+// }
+  
+  
+  
+//   show(){  
+//     push();
+//     noStroke();
+//   fill(150,50,50,50);
+//  translate(width/2,height/2);
+//     rotate(PI/3);
+//     ellipse(0,30,20,80);
+//     pop();
+//   }
+//   // for (let i = 0; i < 10; i ++) {
+//   // ellipse(this.pos.x, this.pos.y, 20, 80);
+//   // rotate(PI/3);
+//   //}
+  
+//   applyForce(force){
+//     let f = p5.Vector.div(force,this.mass);
+//     this.acc.add(f);
+//   }
+  
+  
+//   update(){
+//     this.vel.add(this.acc);
+//     this.pos.add(this.vel);
+//     this.acc.set(0,0);
+//   }
+// }
+
 let flower;
 
+let degree;
+let framerate;
 
-function setup() {
-  createCanvas(400, 400);
-  flower = new walker();
-}
-
-function draw() {
-  background(50);
-  
-  flower.show();
-  let wind = createVector(0.1,0);
-  flower.applyForce(wind);
-  
-  let gravity = createVector(0,0.2);
-  
-  let weight=p5.Vector.mult(gravity, flower.mass);
-  
-  flower.applyForce(weight);
-}
-
-class walker {
-  
-  
-  
-  constructor() {
-  this.pos = createVector(width/2,height/2);
-  this.vel = createVector(0,0);
-  this.acc = createVector(0,0);
-}
-  
-  
-  
-  show(){
-  translate(200, 200);  
+function setup(){
+  createCanvas(400,400);
+  ellipse(CENTER);
   noStroke();
-  fill(150,50,50,50);
-  for (let i = 0; i < 10; i ++) {
-  ellipse(0, 30, 20, 80);
-  rotate(PI/3);
-  }
-  }
+  degree=0;
+  framerate=30;
   
-  applyForce(force){
-    let f = p5.Vector.div(force,this.mass);
-    this.acc.add(f);
-  }
-  
-  
-  update(){
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.set(0,0);
-    
-  }
+  flower = new walker(200,200);
 }
+
+function draw(){
+  background(0);
+  fill(255);
+  frameRate(framerate);
+  flower.turnEllipse();
+}
+
+class walker{
+  constructor(x,y){
+    this.pos = createVector(width/2,height/2);
+  }
+  
+  turnEllipse(){
+    fill(0,255,0,30);
+    push();
+    translate(width/2,height/2);
+    
+    if(mouseIsPressed){
+      rotate(++degree);
+    }else{
+      rotate(0);
+    }
+    ellipse(0,30,20,80);
+    pop();
+  }
+} 
   
