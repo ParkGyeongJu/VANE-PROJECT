4c24dac132cafc90d725b6ea2eca3ce246d76a4a
ParkGyeongJu <anauddl414@naver.com>
2022-11-07 22:04:58 +0900

    중력더하기

--- sketch.js
+++ sketch.js
@@ -10,14 +10,28 @@ function draw() {
   background(50);
   
   flower.show();
+  let wind = createVector(0.1,0);
+  flower.applyForce(wind);
+  
+  let gravity = createVector(0,0.2);
+  
+  let weight=p5.Vector.mult(gravity, flower.mass);
+  
+  flower.applyForce(weight);
 }
 
 class walker {
+  
+  
+  
   constructor() {
   this.pos = createVector(width/2,height/2);
   this.vel = createVector(0,0);
   this.acc = createVector(0,0);
 }
+  
+  
+  
   show(){
   translate(200, 200);  
   noStroke();
@@ -27,4 +41,18 @@ class walker {
   rotate(PI/3);
   }
   }
+  
+  applyForce(force){
+    let f = p5.Vector.div(force,this.mass);
+    this.acc.add(f);
+  }
+  
+  
+  update(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0,0);
+    
+  }
 }
+  
